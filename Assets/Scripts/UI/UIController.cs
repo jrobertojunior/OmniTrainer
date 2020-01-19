4d130e87210423ffb315ca402013c1efed59c66b
Jose Roberto <jrfsj@cin.ufpe.br>
2020-01-19 09:49:04 -0300

    working fake presentation

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -8,9 +8,13 @@ public class UIController : MonoBehaviour
     public GameObject teachingCard;
     public GameObject usedCard;
 
+    float offsetx = -195;
+
     float offset1 = 135;
-    float offset2 = 0;
-    float offset3 = -135;
+    float offset2 = -40;
+    float offset3 = -215;
+
+    public bool skipOne = false;
 
     int state = 0;
     int cardType = 0;
@@ -23,35 +27,18 @@ public class UIController : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            switch (state)
-            {
-                case 0:
-                    CreateCard(slot: 0, type: cardType);
-                    break;
-                case 1:
-                    CreateCard(slot: 1, type: cardType);
-                    break;
-                case 2:
-                    CreateCard(slot: 2, type: cardType);
-                    break;
-            }
-            if (state < 3)
-            {
-                state += 1;
-            } else
-            {
-                state = 0;
-                cardType += 1;
-            }
+            ChangeStateCard1();
         }
     }
 
-    void CreateCard(GameObject cardPrefab, float offset)
+    GameObject CreateCard(GameObject cardPrefab, float offset)
     {
-        var card = Instantiate(cardPrefab, new Vector3(0, offset, 0), Quaternion.identity);
+        var card = Instantiate(cardPrefab, new Vector3(offsetx, offset, 0), Quaternion.identity);
         card.transform.SetParent(transform, false);
+
+        return card;
     }
 
     void CreateCard(int slot, int type)
@@ -82,6 +69,41 @@ public class UIController : MonoBehaviour
             case 2:
                 CreateCard(usedCard, offset);
                 break;
+        }
+    }
+
+
+    int card1State = 0;
+    bool disableCard1 = false;
+    public void ChangeStateCard1()
+    {
+        if (skipOne)
+        {
+            skipOne = false;
+        }
+        else
+        {
+            if (!disableCard1)
+            {
+                switch (card1State)
+                {
+                    case 0:
+                        notificationCard.SetActive(true);
+                        card1State += 1;
+                        break;
+                    case 1:
+                        notificationCard.SetActive(false);
+                        teachingCard.SetActive(true);
+                        card1State += 1;
+                        break;
+                    case 2:
+                        teachingCard.SetActive(false);
+                        usedCard.SetActive(true);
+                        card1State = 0;
+                        disableCard1 = true;
+                        break;
+                }
+            }
         }
     }
 }
